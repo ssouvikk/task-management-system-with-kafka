08ac700fe1be6b0167e97cd95eb094a671136e2d
ssouvikk <souvik.edu.mondal@gmail.com>
2025-02-14 00:10:56 +0530

    nextJS is added

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
